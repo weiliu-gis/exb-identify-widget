695084e475acad92c046a4a05c1425ceceecdae3
Wei Liu <wliu54@buffalo.edu>
2025-03-31 16:01:34 -0400

    Create README.md

---
+++ README.md
@@ -0,0 +1,94 @@
+# Introduction
+
+One of our most popular web map application relies heavily on the "Identify" widget from ArcGIS WebApp Builder. Since WebApp Builder will retire soon, it's necessary to re-build our old App with ArcGIS Experience Builder. The same fucntionality provided by "Identify" does not come with Experience Builder (at least at the time I write this). So the idea strikes us that we can use a custom widget ourselves.
+
+The major part of the idea and the code for [this custom widget](https://community.esri.com/t5/experience-builder-custom-widgets/identify-for-experience-builder-1-14/tac-p/1594245#M232) is introduced by [@JeffreyThompson2](https://community.esri.com/t5/user/viewprofilepage/user-id/677423) from Esri community group of Experience Builder Custom Widgets. Shared as below, the two posts by Jeffery are really helpful for understanding the design logic of the widget.
+
+- [Identify Widget from Mouse Click or Search Widget (First version of the custom Identify Widget)](https://community.esri.com/t5/experience-builder-custom-widgets/identify-widget-from-mouse-click-or-search-widget/tac-p/1596103#M234)
+- [Identify for Experience Builder 1.14 (The updated Version)](https://community.esri.com/t5/experience-builder-custom-widgets/identify-for-experience-builder-1-14/ta-p/1480343)
+
+# Use this widget
+
+ArcGIS Experience Builder Developer Edition is required. See documentation by Esri to establish the development environment [here](https://developers.arcgis.com/experience-builder/guide/install-guide/).
+
+To add custom widgets to the development environment, copy and paste the widget folders to `./ArcGISExperienceBuilder/client/your-extensions/widgets`. (Read more [here](https://developers.arcgis.com/experience-builder/guide/getting-started-widget/))
+
+It's important to mention that the "Identify" actually consists of two widgets: [identify-custom](https://github.com/weiliu-gis/exb-identify-widget/tree/master/identify-custom) and [search-sendToIdentify](https://github.com/weiliu-gis/exb-identify-widget/tree/master/search-sendToIdentify). So it's necessary to compile both widgets from `./ArcGISExperienceBuilder/client/your-extensions/widgets` by running `npm run build:prod` in the command window for client.
+
+# Modifications
+
+To make the re-built App look and function similarly to the old App, I modified the code in the following ways:
+
+#### 1. Instead of showing fueature layers' information in a tab view, a scrollable page view is applied.
+
+The code that controls the layout of Identify's results is in `./identify-custom/src/components/results.tsx`. To keep the tab view, use:
+
+```TypeScript
+<Tabs
+    fill
+    type='underline'
+    scrollable>
+    {message ? message.map(div => {
+        const markup = { __html: div.innerHTML }
+        if (div.children[0]?.children[0].innerText) {
+            return < Tab title={div.children[0]?.children[0].innerText} id={div.id} >
+                <div dangerouslySetInnerHTML={markup}></div>
+            </Tab>
+        }
+    }) : <></>}
+</Tabs>
+```
+
+To switch to the scrollable page view, use:
+
+```TypeScript
+<div style={{ overflowY: 'auto', padding: '10px', border: '1px solid #ffffff' }}>
+    {message ? message.map((div, index) => {
+        const markup = { __html: div.innerHTML }
+        // Check if the message content is empty
+        if (!div.innerHTML.trim()) {
+            return null;
+        }
+
+        return (
+            <div key={index} style={{ marginBottom: '20px', paddingBottom: '10px', borderBottom: '1px solid #ffffff' }}>
+                <div dangerouslySetInnerHTML={markup}></div>
+            </div>
+        )
+    }) : <p>No results found.</p>}
+</div>
+```
+
+#### 2. The updated Search Widget has basically the same functionality as in Jeffery's original code, but was re-developed based on ExB developer edition version 1.15 instead of 1.12. (I was not able to figure out why the older version is not working... Fortunately, it worked with v1.15)
+
+Read [this post](https://community.esri.com/t5/experience-builder-custom-widgets/identify-widget-from-mouse-click-or-search-widget/tac-p/1596103#M234) to learn more about the custom Search Widget.
+
+#### 3. Upon a "click" event, instead of highlighting all selected layers, only allow one layer to be highlighted.
+
+In our scenario, it's better user experience to only highlight one specific layer when a user clicks somewhere in the map. So in `./identify-custom/src/components/identify.tsx`, the change has been made as:
+
+```TypeScript
+if (
+    layerView.layer.type === "feature" &&
+    typeof layerView.highlight === "function" &&
+  layer.title === "TITLE_OF_LAYER_TO_BE_HIGHTLIGHTED" // Only allow highlighting the layer with this title
+) {
+    const newHighlight = handles.add(layerView.highlight(graphic));
+    highlights.current.push(newHighlight);
+    highlighted = true;
+}
+```
+
+#### 4. Remove empty results from the Identify's result panel.
+
+In `./identify-custom/src/components/results.tsx`:
+
+```TypeScript
+if (!div.innerHTML.trim()) {
+    return null; // Skip rendering this popup if it's empty
+}
+```
+
+# Limitation
+
+The Search Widget delays the reponses quite a bit on the layer source search requests. It works normally with locator source search attempts.
--- identify-custom/src/components/identify.tsx
+++ identify-custom/src/components/identify.tsx
@@ -1,138 +1,143 @@
-import { React, appActions, getAppStore } from 'jimu-core'
-import Feature from 'esri/widgets/Feature'
-import Handles from 'esri/core/Handles'
-import Results from './results'
+import { React, appActions, getAppStore } from "jimu-core";
+import Feature from "esri/widgets/Feature";
+import Handles from "esri/core/Handles";
+import Results from "./results";
 
-const { useRef, useState, useEffect } = React
+const { useRef, useState, useEffect } = React;
 
 const Identify = (props) => {
-
-    //Clears any highlights created by this widget
-    const clearHighlights = () => {
-        // console.log("Before clearing highlights:", highlights.current.length) ////////////////////////////////////
-        for (let i = 0; i < highlights.current.length; i++) {
-            highlights.current[i].remove()
-        }
-        highlights.current = []
+  //Clears any highlights created by this widget
+  const clearHighlights = () => {
+    // console.log("Before clearing highlights:", highlights.current.length) ////////////////////////////////////
+    for (let i = 0; i < highlights.current.length; i++) {
+      highlights.current[i].remove();
     }
+    highlights.current = [];
+  };
 
-    //This call to clearHighlights may not be necessary
-    try {
-        clearHighlights()
-    } catch {
-        //Do nothing
+  //This call to clearHighlights may not be necessary
+  try {
+    clearHighlights();
+  } catch {
+    //Do nothing
+  }
+
+  const highlights = useRef([]);
+  const tempHtmlPackages = useRef([]);
+  const [htmlPackages, setHtmlPackages] = useState([]);
+
+  const event = props.event;
+  const featureContainer = props.featureContainer;
+  const view = props.mapView.view;
+  const sidebarId = props.sidebarId;
+  let highlighted = false;
+
+  //When switch clicked off remove highlights and clear results panel
+  useEffect(() => {
+    if (!props.on) {
+      clearHighlights();
+      tempHtmlPackages.current = [];
+      setHtmlPackages(tempHtmlPackages.current);
     }
+  }, [props.on]);
 
-    const highlights = useRef([])
-    const tempHtmlPackages = useRef([])
-    const [htmlPackages, setHtmlPackages] = useState([])
-
-    const event = props.event
-    const featureContainer = props.featureContainer
-    const view = props.mapView.view
-    const sidebarId = props.sidebarId
-    let highlighted = false
-
-    //When switch clicked off remove highlights and clear results panel
-    useEffect(() => {
-        if (!props.on) {
-            clearHighlights()
-            tempHtmlPackages.current = []
-            setHtmlPackages(tempHtmlPackages.current)
+  //When a new event occurs clear the old highlights and fetch the new features
+  useEffect(() => {
+    view.popup.fetchFeatures(event).then(async (response) => {
+      clearHighlights();
+      tempHtmlPackages.current = [];
+      let handles = new Handles();
+      //await features before proceeding
+      const data = await response.allGraphicsPromise;
+      const layers = [];
+      if (data.length > 0) {
+        //Loop through results and find all the unique layers
+        for (let i = 0; i < data.length; i++) {
+          if (i === 0) {
+            layers.push(data[i].layer);
+          } else if (!layers.includes(data[i].layer)) {
+            layers.push(data[i].layer);
+          }
         }
-    }, [props.on])
-
-    //When a new event occurs clear the old highlights and fetch the new features
-    useEffect(() => {
-        view.popup.fetchFeatures(event).then(async response => {
-            clearHighlights()
-            tempHtmlPackages.current = []
-            let handles = new Handles()
-            //await features before proceeding
-            const data = await response.allGraphicsPromise
-            const layers = []
-            if (data.length > 0) {
-                //Loop through results and find all the unique layers
-                for (let i = 0; i < data.length; i++) {
-                    if (i === 0) {
-                        layers.push(data[i].layer)
-                    } else if (!layers.includes(data[i].layer)) {
-                        layers.push(data[i].layer)
-                    }
-                }
-                //Loop through the layers.
-                layers.forEach(async (layer, i) => {
-                    const layerView = await view.whenLayerView(layer)
-                    //Create a div to store the results with a title.
-                    const groupDiv = document.createElement("div")
-                    groupDiv.className = "container"
-                    const layerTitle = document.createElement("h3")
-                    layerTitle.innerHTML = layer.title
-                    groupDiv.appendChild(layerTitle)
-                    featureContainer.appendChild(groupDiv)
-                    //Loop trough the graphics
-                    data.forEach((graphic, j) => {
-                        //If the graphic comes from the current layer, process the results
-                        if (graphic.layer === layer) {
-                            //Highlight the feature
-                            if (
-                                layerView.layer.type === "feature" &&
-                                typeof layerView.highlight === "function" &&
-                                layer.title === "Tax Parcel"
-                            ) {
-                                // console.log("Event3:", highlights.current.length) /////////////////////////////////////
-                                // console.log(highlights.current.length); ////////////////////////////////////////////////////
-                                const newHighlight = handles.add(layerView.highlight(graphic))
-                                highlights.current.push(newHighlight)
-                                highlighted = true
-                                // console.log(highlights.current.length); ////////////////////////////////////////////////////
-                            }
-                            //Create a Feature widget from the API for each graphic
-                            const featureChild = new Feature({
-                                container: document.createElement("div"),
-                                graphic: graphic,
-                                map: view.map,
-                                spatialReference: view.spatialReference,
-                                defaultPopupTemplateEnabled: true
-                            })
-                            //Package the results to pass down to Results
-                            groupDiv.appendChild(featureChild.container)
-                            const htmlPackage = document.createElement("div")
-                            htmlPackage.id = `resultContainer${i}_${j}`
-                            htmlPackage.appendChild(groupDiv)
-                            tempHtmlPackages.current.push(htmlPackage)
-                        }
-                    })
-                })
+        //Loop through the layers.
+        layers.forEach(async (layer, i) => {
+          const layerView = await view.whenLayerView(layer);
+          //Create a div to store the results with a title.
+          const groupDiv = document.createElement("div");
+          groupDiv.className = "container";
+          const layerTitle = document.createElement("h3");
+          layerTitle.innerHTML = layer.title;
+          groupDiv.appendChild(layerTitle);
+          featureContainer.appendChild(groupDiv);
+          //Loop trough the graphics
+          data.forEach((graphic, j) => {
+            //If the graphic comes from the current layer, process the results
+            if (graphic.layer === layer) {
+              //Highlight the feature
+              if (
+                layerView.layer.type === "feature" &&
+                typeof layerView.highlight === "function" &&
+                layer.title === "TITLE_OF_LAYER_TO_BE_HIGHTLIGHTED"
+              ) {
+                // console.log("Event3:", highlights.current.length) /////////////////////////////////////
+                // console.log(highlights.current.length); ////////////////////////////////////////////////////
+                const newHighlight = handles.add(layerView.highlight(graphic));
+                highlights.current.push(newHighlight);
+                highlighted = true;
+                // console.log(highlights.current.length); ////////////////////////////////////////////////////
+              }
+              //Create a Feature widget from the API for each graphic
+              const featureChild = new Feature({
+                container: document.createElement("div"),
+                graphic: graphic,
+                map: view.map,
+                spatialReference: view.spatialReference,
+                defaultPopupTemplateEnabled: true,
+              });
+              //Package the results to pass down to Results
+              groupDiv.appendChild(featureChild.container);
+              const htmlPackage = document.createElement("div");
+              htmlPackage.id = `resultContainer${i}_${j}`;
+              htmlPackage.appendChild(groupDiv);
+              tempHtmlPackages.current.push(htmlPackage);
             }
-            //Timeout to allow for data to be written to memory.
-            //After timeout length, set HtmlPackages to tempHtmlPackages, triggering Results to re-render. If nothing was highlighted, clear old highlights else open the sidebar to reveal new results
-            setTimeout(() => {
-                setHtmlPackages(tempHtmlPackages.current)
-                if (!highlighted) {
-                    clearHighlights()
-                } else {
-                    getAppStore().dispatch(appActions.widgetStatePropChange(`widget_${sidebarId}`, 'collapse', true))
-                }
-            }, 1000)
-            
-        })
-    }, [event])
+          });
+        });
+      }
+      //Timeout to allow for data to be written to memory.
+      //After timeout length, set HtmlPackages to tempHtmlPackages, triggering Results to re-render. If nothing was highlighted, clear old highlights else open the sidebar to reveal new results
+      setTimeout(() => {
+        setHtmlPackages(tempHtmlPackages.current);
+        if (!highlighted) {
+          clearHighlights();
+        } else {
+          getAppStore().dispatch(
+            appActions.widgetStatePropChange(
+              `widget_${sidebarId}`,
+              "collapse",
+              true
+            )
+          );
+        }
+      }, 1000);
+    });
+  }, [event]);
 
-
-    return (
+  return (
+    <div>
+      {htmlPackages.length > 0 ? (
+        <Results message={htmlPackages}></Results>
+      ) : (
         <div>
-            {htmlPackages.length > 0 ?
-                <Results message={htmlPackages}></Results> :
-                <div>
-                    {props.on ?
-                         <p>&nbsp;&nbsp;No Results Found</p> 
-                        : <p>&nbsp;&nbsp;Enable switch to identify features.</p>
-                    }
-                </div>
-             }
+          {props.on ? (
+            <p>&nbsp;&nbsp;No Results Found</p>
+          ) : (
+            <p>&nbsp;&nbsp;Enable switch to identify features.</p>
+          )}
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
-export default Identify
+export default Identify;
